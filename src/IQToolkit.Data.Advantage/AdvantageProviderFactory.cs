2019a8f3e7b12064994c781cb15004f1e1a12b9f
Basile MELLAC <b.mellac@intrapole.fr>
2026-02-16 09:00:56 +0100

    - ads provider

--- src/IQToolkit.Data.Advantage/AdvantageProviderFactory.cs
+++ src/IQToolkit.Data.Advantage/AdvantageProviderFactory.cs
@@ -7,8 +7,20 @@ namespace IQToolkit.Data.Advantage
     {
         private static readonly Lazy<DbProviderFactory> _factory = new Lazy<DbProviderFactory>(() =>
         {
-            // Dynamically load the Advantage.Data.Provider factory
-            var adsFactoryType = Type.GetType("Advantage.Data.Provider.AdsFactory, Advantage.Data.Provider", throwOnError: true);
+			// Dynamically load the Advantage.Data.Provider factory
+			try
+			{
+				var assembly = System.Reflection.Assembly.LoadFrom("Advantage.Data.Provider.dll");
+				var type = assembly.GetType("Advantage.Data.Provider.AdsFactory");
+				return (DbProviderFactory)Activator.CreateInstance(type);
+			}
+			catch (Exception ex)
+			{
+				// Check ex.FusionLog or ex.LoaderExceptions
+				Console.WriteLine(ex.ToString());
+			}
+
+		var adsFactoryType = Type.GetType("Advantage.Data.Provider.AdsFactory, Advantage.Data.Provider", throwOnError: true);
             return (DbProviderFactory)Activator.CreateInstance(adsFactoryType);
         });
 
